f3d527d66c3c583432afcbe97f0a25b72ddbda02
YulHee Kim <dbfgml980413@gmail.com>
2021-03-09 15:51:32 +0900

    개발:: 코드 수정

--- demo_image.py
+++ demo_image.py
@@ -22,9 +22,11 @@ n_classes = 7
 model = pspunet((IMG_HEIGHT, IMG_WIDTH, 3), n_classes)
 model.load_weights("pspunet_weight.h5")
 
-img = cv2.imread('./surface_img/data1.jpg')
+img = cv2.imread('./surface_img/data1.jpeg')
 img = cv2.resize(img, (IMG_WIDTH,IMG_HEIGHT))
 img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+img = img / 255
 
 img = tf.expand_dims(img, 0)
 pre = model.predict(img)
