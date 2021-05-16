b168bc45ea9179bf3302d9733505b54f41f87336
YulHee Kim <dbfgml980413@gmail.com>
2021-05-16 15:30:08 +0900

    caution zone detecting

--- converted_test.py
+++ converted_test.py
@@ -1,9 +1,9 @@
 import cv2
 import tensorflow as tf
-from data_loader.display import create_mask
+from data_loader.display import create_mask, show_predictions
 import numpy as np
 from PIL import Image
-
+import pandas as pd
 
 gpus = tf.config.experimental.list_physical_devices('GPU')
 
@@ -43,7 +43,7 @@ img = img / 255
 img = tf.expand_dims(img, 0)
 
 input_data = np.array(img, dtype=np.float32)
-
+#print(input_data.shape)
 '''
 Get indexes of input and output layers
 input_details[0]['index']를 출력하면 0, 딕셔너리 안에 index라는 key가 있고 그 Index값이 0임.
@@ -64,10 +64,110 @@ interpreter.invoke()
 output_data = interpreter.get_tensor(output_details[0]['index'])
 
 pre = create_mask(output_data).numpy()
-print(pre)
 
-# for x in pre:
-#     for i in x:
-#         print(i, end=' ')
-#     print()
+#result1 = np.array(pre).T[0][0]
+result1 = np.array(pre).T[0][0:80]
+result2 = np.array(pre).T[0][80:160]
+result3 = np.array(pre).T[0][160:240]
+result4 = np.array(pre).T[0][240:320]
+result5 = np.array(pre).T[0][320:400]
+result6 = np.array(pre).T[0][400:480]
 
+print(result1)
+print("=================================")
+print(result2)
+caution1_2 = []
+caution1_3 = []
+caution1_5 = []
+for j in result1:
+    for x in j:
+        if x == 3:
+            caution1_3.append(j)
+        elif x == 2:
+            caution1_2.append(j)
+        elif x == 5:
+            caution1_5.append(j)
+
+print("label2: ", len(caution1_2))
+print("label3: ", len(caution1_3))
+print("label5: ", len(caution1_5))
+
+caution2_2 = []
+caution2_3 = []
+caution2_5 = []
+for j in result2:
+    for x in j:
+        if x == 3:
+            caution2_3.append(j)
+        elif x == 2:
+            caution2_2.append(j)
+        elif x == 5:
+            caution2_5.append(j)
+
+print("label2: ", len(caution2_2))
+print("label3: ", len(caution2_3))
+print("label5: ", len(caution2_5))
+
+caution3_2 = []
+caution3_3 = []
+caution3_5 = []
+for j in result2:
+    for x in j:
+        if x == 3:
+            caution3_3.append(j)
+        elif x == 2:
+            caution3_2.append(j)
+        elif x == 5:
+            caution3_5.append(j)
+
+print("label2: ", len(caution3_2))
+print("label2: ", len(caution3_3))
+print("label5: ", len(caution3_5))
+
+caution4_2 = []
+caution4_3 = []
+caution4_5 = []
+for j in result2:
+    for x in j:
+        if x == 3:
+            caution4_3.append(j)
+        elif x == 2:
+            caution4_2.append(j)
+        elif x == 5:
+            caution4_5.append(j)
+
+print("label2: ", len(caution4_2))
+print("label2: ", len(caution4_3))
+print("label5: ", len(caution4_5))
+
+caution5_2 = []
+caution5_3 = []
+caution5_5 = []
+for j in result2:
+    for x in j:
+        if x == 3:
+            caution5_3.append(j)
+        elif x == 2:
+            caution5_2.append(j)
+        elif x == 5:
+            caution5_5.append(j)
+
+print("label2: ", len(caution5_2))
+print("label2: ", len(caution5_3))
+print("label5: ", len(caution5_5))
+
+caution6_2 = []
+caution6_3 = []
+caution6_5 = []
+for j in result2:
+    for x in j:
+        if x == 3:
+            caution6_3.append(j)
+        elif x == 2:
+            caution6_2.append(j)
+        elif x == 5:
+            caution6_5.append(j)
+
+print("label2: ", len(caution6_2))
+print("label2: ", len(caution6_3))
+print("label5: ", len(caution6_5))
